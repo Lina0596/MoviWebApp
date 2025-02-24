00c83aeb1a272ef42870f205316b81239699852c
Lina0596 <lina.dahlhaus@icloud.com>
2025-02-24 22:17:22 +0100

    implement route add_movie

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from flask import Flask, request, url_for, render_template, flash, redirect
 from MoviWebApp.datamanager.sqlite_data_manager import SQLiteDataManager
+import requests
 
 
 app = Flask(__name__)
+app.secret_key = '0944e3979fa07a3ab609cd515cbcc687'
 data_manager = SQLiteDataManager('moviwebapp.db')
+
+
+def get_movie_from_api(title):
+    api_key = '93630ab7'
+    url = f"http://www.omdbapi.com/?apikey={api_key}&t={title}"
+    request_movie = requests.get(url)
+    movie = request_movie.json()
+    return movie
 
 
 @app.route('/')
@@ -17,10 +27,10 @@ def list_users():
     return render_template('users.html', users=users)
 
 
-@app.route('/users/<user_id>', methods=['GET'])
+@app.route('/users/<int:user_id>', methods=['GET'])
 def list_user_movies(user_id):
     user_movies = data_manager.get_user_movies(user_id)
-    return render_template('movies.html', movies=user_movies)
+    return render_template('movies.html', movies=user_movies, user_id=user_id)
 
 
 @app.route('/add_user', methods=['GET', 'POST'])
@@ -31,17 +41,25 @@ def add_user():
         return redirect(url_for('list_users'))
     return render_template('add_user.html')
 
-@app.route('/users/<user_id>/add_movie')
+@app.route('/users/<int:user_id>/add_movie', methods=['GET', 'POST'])
 def add_movie(user_id):
-    pass
+    if request.method == 'POST':
+        title = request.form['title']
+        movie = get_movie_from_api(title)
+        if movie["Response"] == "True":
+            data_manager.add_movie(movie, user_id)
+            return redirect(url_for('list_user_movies', user_id=user_id))
+        flash('This movie does not exist.', 'error')
+        return redirect(url_for('add_movie', user_id=user_id))
+    return render_template('add_movie.html')
 
 
-@app.route('/users/<user_id>/update_movie/<movie_id>')
+@app.route('/users/<int:user_id>/update_movie/<int:movie_id>')
 def update_movie(user_id, movie_id):
     pass
 
 
-@app.route('/users/<user_id>/delete_movie/<movie_id>')
+@app.route('/users/<int:user_id>/delete_movie/<int:movie_id>')
 def delete_movie(movie_id):
     pass
 
